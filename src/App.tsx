ec3de4a428dee142f9c5a4177b591ec3cdfc70d9
Adrián Méndez <adrian@bincat.me>
2022-02-27 15:10:48 -0600

    Creating .env example and  cleaning file.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { Link } from "react-router-dom";
 import logo from "./assets/logo.png";
-import { generateSpotifyUrl } from "./services/Spotify";
 import "./styles/App.css";
-import { openAuthenticationPopup } from "./utils/openAuthenticationPopup";
 
 function App() {
 
@@ -10,9 +8,9 @@ function App() {
     <div className="flex justify-center items-center flex-col">
       <img className="max-w-full mb-5" src={logo} />
       <div className="flex justify-between">
-        <button className="btn btn-primary flex-1">
-          Link Youtube Music Account
-        </button>
+        <Link to="/ymusic" className="btn btn-primary flex-1 ml-5">
+        Link Youtube Music Account
+        </Link>
 
         <Link to="/spotify" className="btn btn-secondary flex-1 ml-5">
           Link Spotify Account
