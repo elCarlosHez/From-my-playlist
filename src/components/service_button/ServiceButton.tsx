0fe533378d61b8ea414fb3ab01f52e7654ac467b
Carlos Arellano <luis.harellano@alumnos.udg.mx>
2022-03-10 13:14:11 -0600

    feat: Service button and Choose Service component

---
+++ src/components/service_button/ServiceButton.tsx
@@ -0,0 +1,29 @@
+import { Fragment, lazy, useEffect, useState } from "react";
+import { useAppContext } from "../../contexts/AppContext";
+import { ServiceBGColors, ServicesList } from "../../types/ServicesList";
+
+interface IServiceButton {
+  onPress: () => void;
+  service: ServicesList;
+}
+
+export const ServiceButton = (props: IServiceButton): JSX.Element => {
+  const { onPress, service } = props;
+  const [logo, setLogo] = useState("");
+
+  useEffect(() => {
+    getLogo();
+  }, []);
+
+  const getLogo = async () => {
+    const module = await import(`../../assets/brands/${service}.png`);
+    const logo = module.default;
+    setLogo(logo as string);
+  };
+
+  return (
+    <button className="h-full w-full py-2" onClick={onPress}>
+      <img className="max-h-16 m-auto" src={logo} alt={service} />
+    </button>
+  );
+};
